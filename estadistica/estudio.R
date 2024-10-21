cdcd1c44f4e25b20a5fd91164cc65568f9397562
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2024-10-22 00:02:12 +0200

    .

--- estadistica/estudio.R
+++ estadistica/estudio.R
@@ -5,3 +5,28 @@ f5 <- function(x,y){
 }
 f5 = Vectorize(f5)
 c= 1/(1+2+3+2+4+6+3+6+9)
+
+#8
+pbinom(30,400,0.1)
+pbinom(50,400,0.1) - pbinom(29,400,0.1)
+1-pbinom(64,400,0.1)
+#9
+choose(5,3)*3*2
+choose(5,2)*4
+
+#11
+fu <- function(x){
+  ifelse(x<0,0,3*exp(-3*x))
+}
+fu = Vectorize(fu)
+integrate(fu,0,1)
+1-exp(-3)
+
+#12
+fu <- function(x){
+  if(x < 0) 0
+  if(2 <= x & x <= 3) x
+  if(1 <= x & x <= 2) x^2
+}
+fu = Vectorize(fu)
+integrate(fu,-Inf,Inf)$value

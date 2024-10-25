e12a01f8e0b7fce5b6abc67b72f3b3a1cd3dec0b
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2024-10-25 10:14:38 +0200

    .

--- estadistica/estudio.R
+++ estadistica/estudio.R
@@ -24,9 +24,46 @@ integrate(fu,0,1)
 
 #12
 fu <- function(x){
-  if(x < 0) 0
-  if(2 <= x & x <= 3) x
-  if(1 <= x & x <= 2) x^2
+  if(1 <= x & x <= 2) return ((6/29) * (x^2))
+  if(2 <= x & x <= 3) return ((6/29) * x)
+  if(x < 0) return (0)
+  else return (0)
 }
 fu = Vectorize(fu)
-integrate(fu,-Inf,Inf)$value
+c = 1/(integrate(fu,1,2)$value +integrate(fu,2,3)$value)
+integrate(fu,2,Inf)$value
+integrate(fu,1,1.5)$value
+19/116
+#15
+fu <- function(x){
+  if(x >= 27) k*27^4/x^2
+  else k*x^2
+}
+fu = Vectorize(fu)
+integrate(fu,0,100)$value
+p = 0 
+i = 100
+while (p < 0.95) {
+   i = i+1
+   p = integrate(fu,0,i)$value
+}
+#406
+integrate(fu,0,405)
+#k = 1/integrate(fu,0,Inf)$value
+
+#16
+fu <- function(x,y){
+  if(x <= 1 & y <= 1) c*(x^2 + y^2)
+  else 0
+}
+fu = Vectorize(fu)
+
+integrate2_dxdy(fu,function(y)0,function(y)0.5,0.5,1)
+
+integrate2_dxdy(fu,function(y)0.25,function(y)0.75,0,1)
+29/64
+
+integrate2_dxdy(fu,function(y)0,function(y)1,0,0.5)
+5/16
+
+  #c = 1/integrate2_dxdy(fu,function(y)0,function(y)1,0,1)$value

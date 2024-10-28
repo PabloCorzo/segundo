9351e6c0867d79ffc2a3b95b55ab732cd45fad3f
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2024-10-28 10:02:47 +0100

    .

--- estadistica/estudio.R
+++ estadistica/estudio.R
@@ -67,3 +67,76 @@ integrate2_dxdy(fu,function(y)0,function(y)1,0,0.5)
 5/16
 
   #c = 1/integrate2_dxdy(fu,function(y)0,function(y)1,0,1)$value
+#19
+
+f19 <- function(x,y){
+  if(x < 4 & (y-1) < 4 & (x+y) < 3) (x*y)/96
+  else 0
+}
+f19 <- Vectorize(f19)
+integrate2_dxdy(f19,function(y)0,function(y)4,1,5)
+1/48
+
+#20
+f20 <- function(x,y){
+  c * exp(-2*x) * exp(-3*y)
+}
+f20 <- Vectorize(f20)
+#c = 6
+#integrate2_dxdy(f20,function(y)1,function(y)3,0,Inf)$value
+#integrate2_dydx(f20,3,Inf,function(x)0,function(x)(x-3))$value
+
+#21
+f21 <- function(x,y){
+  if(x <= 1 & y <= 2) (3/5) *x*(x+y)
+  else 0
+}
+f21 <- Vectorize(f21)
+f21_x <- function(x){
+  if(x <= 2) integrate(f21(x = x, y), 0, 2)$value
+  else 0
+}
+f21_x <- Vectorize(f21_x)
+EX = integrate(function(x) x * f21_x(x), 0, 1)$value
+
+f21_y <- function(y){
+  if(y <= 2) integrate(f21(x,y = y),0,1)$value
+}
+f21_y <- Vectorize(f21_y)
+EY =integrate(function(y) y * f21_y(y), 0, 2)$value
+EXY = EY +EX
+E_XY <- integrate2_dxdy(function(x,y)x * y * f21(x, y), function(y)0, function(y)1, 0, 2)$value
+
+#24
+f24 <- function(x){
+  if(x >= 0)exp(-x)
+  else 0
+}
+f24 <- Vectorize(f24)
+#e^-x representa una distribucion exponencial estandar, de lambda = 1, media 1/lambda y sd = 1/lambda^2
+
+#26
+f26 <- function(x,y){
+  if(x <= 1 & y <= 1) x+y
+  else 0
+}
+f26 <- Vectorize(f26)
+#VAR[X] = E[X^2]-(E[X])^2
+#funcion marginal de x: integrar respecto a y
+fx26 <- function(x){
+  if(x <= 1) x+0.5
+  else 0
+}
+fx26 <- Vectorize(fx26)
+r1 <- integrate(function(x) (x^2 * fx26(x)), 0, 1)$value
+r2 <- (integrate(function(x) (x*fx26(x)),0,1)$value)^2
+result <- r1-r2
+#Cov[X,Y] = E[(X- mux)(Y-muy)]
+E <- sqrt(r2)
+covf <- function(x,y){
+  (x-E)*(y-E)
+}
+covf <- Vectorize(cov)
+cov <- integrate2_dxdy(cov,function(x)0,function(x)1,0,1)$value
+#MULTIPLICAR LAS DOS RAICES DE LAS VARIANZAS; COMO SON IGUALES SE CANCELA Y QUEDA SOLO 1
+corr <- cov/((11/144))
